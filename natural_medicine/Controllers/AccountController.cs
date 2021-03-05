9cbf876f1e7863b18c54ace28cdb37fcad5f6714
Dong Van Hung <>
2021-03-05 12:33:37 +0700

    my account, regiter view

---
+++ natural_medicine/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using natural_medicine.Models;
+
+namespace natural_medicine.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private MyDBContext context = new MyDBContext();
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+        public ActionResult Regiter()
+        {
+            return View();
+        }
+        public ActionResult CheckPhoneNumber(string id)
+        {
+            var result = "";
+            var model = context.users.Where(x => x.phone.Trim() == id).FirstOrDefault();
+            if (model == null)
+            {
+                result = "success";
+            } else
+            {
+                result = "error";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
