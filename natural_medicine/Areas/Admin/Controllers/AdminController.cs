d2ea7fbe44552c62f872bb5c6e78ac5d57d4fb87
Dong Van Hung <>
2021-03-07 00:27:28 +0700

    view login, admin customers

--- natural_medicine/Areas/Admin/Controllers/AdminController.cs
+++ natural_medicine/Areas/Admin/Controllers/AdminController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using natural_medicine.Models;
 
 namespace natural_medicine.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin/Admin
+        private MyDBContext context = new MyDBContext();
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult ViewCustomer(int ?page)
+        {
+            if (page == null) page = 1;
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            var model = context.users.Where(x => x.user_type == 1).ToList();
+            return View(model.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult DeleteCustomer(int id)
+        {
+            var model = context.users.Where(x => x.id == id).FirstOrDefault();
+            context.users.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("ViewCustomer");
         }
     }
 }
