3ce61aa0058c358c7faf94f16d50e155d9cd90a8
Dong Van Hung <>
2021-03-10 15:07:16 +0700

    report

---
+++ natural_medicine/Areas/Admin/Controllers/ReportController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace natural_medicine.Areas.Admin.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Admin/Report
+        public ActionResult ReportImport()
+        {
+            return View();
+        }
+        public ActionResult ReportExport()
+        {
+            return View();
+        }
+        public ActionResult TotalRevenue()
+        {
+            return View();
+        }
+    }
+}
